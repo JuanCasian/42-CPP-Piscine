c0ae12577f20b3955be34d5f5c8c762d41b8588d
Juan Pedro Casian <juanpedrocasian@gmail.com>
2019-01-21 18:37:59 -0800

    correcting files  for ex00

--- Day00/ex00/main.cpp
+++ Day00/ex00/main.cpp
@@ -6,17 +6,40 @@
 /*   By: jcasian <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 15:58:11 by jcasian           #+#    #+#             */
-/*   Updated: 2019/01/21 16:32:21 by jcasian          ###   ########.fr       */
+/*   Updated: 2019/01/21 18:36:36 by jcasian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "Megaphone.class.hpp"
+
+char	to_uppercase(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
 
 int		main(int ac, char **av)
 {
-	Megaphone mega(ac, av);
+	int i;
+	int	j;
 
-	mega.makeNoise();
+	if (ac > 1)
+	{
+		i = 0;
+		while (++i < ac)
+		{
+			j = -1;
+			while (av[i][++j])
+				std::cout << to_uppercase(av[i][j]);
+			if (ac != 2 && i < ac - 1)
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
 	return (0);
 }
