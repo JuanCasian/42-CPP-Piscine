448b524c53af8bc9d8d27642bacafc57beb04d6e
Juan Casian <juanpedrocasian@gmail.com>
2019-05-06 21:21:54 -0700

    adding ex00

---
+++ Days/Day05/ex00/Bureaucrat.cpp
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcasian <jcasian@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/06 20:24:06 by jcasian           #+#    #+#             */
+/*   Updated: 2019/05/06 21:11:04 by jcasian          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+/* Constructors */
+Bureaucrat::Bureaucrat(void) {
+	return ;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name) {
+	if (grade < 1) {
+		throw GradeTooHighException();
+	} else if (grade > 150) {
+		throw GradeTooLowException();
+	}
+	this->_grade = grade;
+	return ;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &src) : _name(src.getName()) {
+	*this = src;
+	return ;
+}
+
+/* Destructor */
+Bureaucrat::~Bureaucrat(void) {
+	return ;
+}
+
+/* Operator */
+Bureaucrat	&Bureaucrat::operator=(Bureaucrat const &rhs) {
+	this->_grade = rhs.getGrade();
+	return *this;
+}
+
+std::ostream	&operator<<(std::ostream &o, Bureaucrat const &rhs) {
+	o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+	return o;
+}
+
+std::ostream	&operator<<(std::ostream &o, Bureaucrat const *rhs) {
+	o << rhs->getName() << ", bureaucrat grade " << rhs->getGrade();
+	return o;
+}
+
+/* Getters and Setters */
+const std::string	Bureaucrat::getName(void) const {
+	return this->_name;
+}
+
+int	Bureaucrat::getGrade(void) const {
+	return this->_grade;
+}
+void	Bureaucrat::setGrade(int grade) {
+	if (grade < 1) {
+		throw GradeTooHighException();
+	} else if (grade > 150) {
+		throw GradeTooLowException();
+	}
+	this->_grade = grade;
+}
+
+/* Functions */
+
+void	Bureaucrat::incrementGrade(int increment) {
+	this->setGrade(this->_grade - increment);
+}
+
+void	Bureaucrat::decrementGrade(int decrement) {
+	this->setGrade(this->_grade + decrement);
+}
+
+/* Exceptions */
+
+Bureaucrat::GradeTooHighException::GradeTooHighException(void) {
+	return ;
+}
+
+Bureaucrat::GradeTooHighException::GradeTooHighException(Bureaucrat::GradeTooHighException const &) {
+	return ;
+}
+
+Bureaucrat::GradeTooHighException::~GradeTooHighException(void) throw() {
+	return ;
+}
+
+Bureaucrat::GradeTooHighException	&Bureaucrat::GradeTooHighException::operator=(Bureaucrat::GradeTooHighException const &) {
+	return *this;
+}
+
+const char	*Bureaucrat::GradeTooHighException::what(void) const throw() {
+	return "The grade that is going to be set is to high!";
+}
+
+/* Constructors */
+Bureaucrat::GradeTooLowException::GradeTooLowException(void) {
+	return ;
+}
+Bureaucrat::GradeTooLowException::GradeTooLowException(Bureaucrat::GradeTooLowException const &) {
+return ;
+}
+/* Destructor */
+Bureaucrat::GradeTooLowException::~GradeTooLowException(void) throw() {
+	return ;
+}
+/* Operator */
+Bureaucrat::GradeTooLowException	&Bureaucrat::GradeTooLowException::operator=(Bureaucrat::GradeTooLowException const &) {
+	return *this;
+}
+const char *Bureaucrat::GradeTooLowException::what(void) const throw() {
+	return "The grade that is going to be set is to low!";
+}
---
+++ Days/Day05/ex00/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcasian <jcasian@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/06 20:57:51 by jcasian           #+#    #+#             */
+/*   Updated: 2019/05/06 21:10:38 by jcasian          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main(void) {
+	Bureaucrat *juan;
+	try {
+		juan = new Bureaucrat("Juan", 160);
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		juan = new Bureaucrat("Juan", 50);
+		std::cout << juan << std::endl;
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		juan->incrementGrade(49);
+		std::cout << juan << std::endl;
+		juan->decrementGrade(149);
+		std::cout << juan << std::endl;
+		juan->incrementGrade(150);
+	} catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
